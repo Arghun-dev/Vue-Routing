136ac7e76e883be2a70965d9077259c3d3af4e90
Arghun-dev <arghun.developer@gmail.com>
2020-03-16 12:14:19 +0330

    Created Project and setted up Routes

---
+++ routing/src/components/About.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class='about'>
+        <h1>About</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
