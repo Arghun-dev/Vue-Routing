fa1962c4add78be01a287063ae358956f86a1abe
Arghun-dev <arghun.developer@gmail.com>
2020-03-16 15:39:12 +0330

    Route Parameters

---
+++ routing/src/components/ViewProfile.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="view-profile">
+        <h1>Profile for {{userId}}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'ViewProfile',
+  data () {
+    return {
+      userId: this.$route.params.user_id
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
